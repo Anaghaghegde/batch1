dbb14317c65d1386008398e815ecccf36e50128d
Anaghaghegde <98947219+Anaghaghegde@users.noreply.github.com>
2022-05-10 16:48:30 +0530

    Add files via upload

--- diji.c
+++ diji.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int n,d[10],p[10];
+int n=5,d[10],p[10];
 int cost[5][5]={{0,99,99,7,99},
                 {3,0,4,99,99},
                 {99,99,0,99,6},
@@ -9,8 +9,7 @@ int cost[5][5]={{0,99,99,7,99},
                 {99,99,99,4,0}};
 void dijik(int source)
 {
- int i,j,u,v,min;
- int s[10];
+ int i,j,u,v,min,s[10];
  for(i=0;i<n;i++)
  {
   d[i]=cost[source][i];
@@ -18,7 +17,7 @@ void dijik(int source)
   p[i]=source;
  }
  s[source]=1;
- for(i=0;i<n;i++)
+ for(i=1;i<n;i++)
  {
   min=99;
   u=-1;
@@ -35,7 +34,87 @@ void dijik(int source)
 
  }/*dijik*/
  
- if(u==-1)
+ if(u==-1)#include<stdio.h>
+#include<stdlib.h>
+int n=5,p[10],d[10];
+int cost[10][10]={{0,99,99,7,99},
+		 {3,0,4,99,99},
+		 {99,99,0,99,6},
+		 {99,2,5,0,99},
+		{99,99,99,4,0}};
+void dijik(int source)
+{
+int i,j,min,u,v,s[10];
+for(i=0;i<n;i++)
+{
+d[i]=cost[source][i];
+s[i]=0;
+p[i]=source;
+}
+s[source]=1;
+for(i=1;i<n;i++)
+{
+min=99;
+u=-1;
+for(j=0;j<n;j++)
+{
+if(s[j]==0)
+{
+if(d[j]<min) //find min
+{
+min=d[j];
+u=j;
+}
+}
+}
+if(u==-1)
+return;
+s[u]=1;
+for(v=0;v<n;v++)
+{
+if(s[v]==0)
+{
+if(d[u]+cost[u][v]<d[v])
+{
+d[v]=d[u]+cost[u][v];
+p[v]=u;
+}
+}
+}
+}}//diji end
+void print_path(int source,int destination)
+{
+int i;
+i=destination;
+while(i!=source)
+{
+printf("%d<--",i);
+i=p[i];
+}
+printf("%d=%d",i,d[destination]);
+}
+void main()
+{
+int i,j,source;
+printf("the cost adjecency:\n"); //read matrix
+for(i=0;i<n;i++){
+	for(j=0;j<n;j++)
+printf("\t%d",cost[i][j]);
+printf("\n"); 
+}
+printf("enter the source vertx:");
+scanf("%d",&source);
+dijik(source);
+printf("the shortest path and distance is:\n");
+for(j=0;j<n;j++)
+{
+if(d[j]==99)
+printf("is not reachable from %d-->%d",source,j);
+else
+print_path(source,j);
+printf("\n");
+}
+}
  return;
  s[u]=1;
  for(v=0;v<n;n++)
@@ -65,9 +144,8 @@ void dijik(int source)
 
 void main()
 {
- int n,i,j,source;
- printf("Number of vertices");
- scanf("%d",&n);
+ int i,j,source;
+
  printf("Enetr the cost adjecency matrix");
   for(i=0;i<n;i++)
    {
